dddb0c5fe4f01089668fd5a2d3f8896ef619d53d
corawym <corawym@gmail.com>
2017-12-04 21:56:31 -0800

    Add config styles

---
+++ js/config/styles.js
@@ -0,0 +1,30 @@
+export const colors = {
+  lightestGreen: '#B5D2D4',
+  backgroundLightGreen: '#81B3B6',
+  mainGreen:'#3C898E',
+  darkGreen: '#0D5355',
+  red: '#FD3C3C',
+  yellow: '#F7B85A',
+  lightYellow: '#F8CF91',
+};
+
+export const typography = {
+  fontMedium: 'Lato-Medium', //body
+  fontHeavy: 'Lato-Heavy',
+  fontBlack: 'Lato-Black',
+};
+
+export const textSizes = {
+  title1: 48,
+  title2: 30, //onboard, registration, home, main title
+  title3: 24,
+  bodyL: 20,
+  body: 18, //main text
+  bodyS:16
+};
+
+export const margins = {
+  marginS: 8,
+  marginM: 16, //screen edge
+  marginL: 22,
+};
