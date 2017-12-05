6dccd4d5e3d29073c1e8fc076cd86dcd5fafe6b5
corawym <corawym@gmail.com>
2017-12-05 13:50:40 -0800

    Set up home scene

---
+++ js/scenes/Home/Home.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+import { View, Text, Image } from 'react-native';
+
+import styles from './styles';
+
+const Home = () => (
+  <View>
+
+  </View>
+);
+
+Home.propTypes = {
+  
+};
+
+export default Home;
