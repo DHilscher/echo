dc715f547200b7349029ca8ec007d2409dad245d
corawym <corawym@gmail.com>
2017-12-05 13:50:40 -0800

    Set up home scene

---
+++ js/scenes/Home/styles.js
@@ -0,0 +1,9 @@
+import { StyleSheet } from 'react-native';
+
+import { colors, typography, textSizes, margins } from '../../config/styles';
+
+const styles = StyleSheet.create({
+
+});
+
+export default styles;
