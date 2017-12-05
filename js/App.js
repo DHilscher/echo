e0ca15d7a12c7f811c91e0b115b750bd80a38355
Mark <marx.affiliate@gmail.com>
2017-12-05 11:02:23 -0800

    Install dependencies, redirected app.js to settings container for test

--- js/App.js
+++ js/App.js
@@ -1,11 +1,13 @@
 import React, { Component } from "react";
 import { Platform, StyleSheet, Text, View } from "react-native";
 
+import SettingsContainer from './scenes/Settings'
+
 export default class App extends Component<{}> {
   render() {
     return (
       <View style={styles.container}>
-        <Text style={styles.welcome}>Welcome to React Native!</Text>
+        <SettingsContainer />
       </View>
     );
   }
