8956f918bf002ef8cbeed699cbec8045264d7b56
corawym <corawym@gmail.com>
2017-12-06 17:15:35 -0800

    Remove unused code

--- js/scenes/Home/HomeContainer.js
+++ js/scenes/Home/HomeContainer.js
@@ -2,24 +2,10 @@ import React, { Component } from 'react';
 import PropTypes from 'prop-types';
 
 import Home from './Home';
-import { Loader } from '../../components/Loader';
 
 class HomeContainer extends Component {
-  state = {  }
-
-  // static propTypes = {
-  //   isLoading: PropTypes.bool.isRequired,
-  // }
-
   render() {
-    // if (isLoading) {
-    //   return (
-    //     <Loader />
-    //   );
-    // }
     return (
-
-      // <Loader />
       <Home />
     );
   }
