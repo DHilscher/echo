44561d0ee076eceb3b8848f6e6634a638d8064eb
corawym <corawym@gmail.com>
2017-12-11 15:56:04 -0800

    Add settings style

---
+++ js/components/Buttons/SmallCustomButton/styles.js
@@ -0,0 +1,24 @@
+import { StyleSheet } from 'react-native';
+
+import { colors, typography, textSizes, margins } from '../../../config/styles';
+
+const styles = StyleSheet.create({
+  buttonWrapper: {
+    alignItems: 'center',
+  },
+  button: {
+    flex: 0,
+    backgroundColor: colors.red,
+    borderRadius: 50,
+  },
+  buttonText: {
+    fontFamily: typography.fontBlack,
+    fontSize: textSizes.body,
+    color: 'white',
+    textAlign: 'center',
+    marginVertical: margins.marginS,
+    marginHorizontal: margins.marginL,
+  },
+});
+
+export default styles;
