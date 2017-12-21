420ea359083efaeaa2588555034b7dffccf7c67d
David Hilscher <davidhilscher@gmail.com>
2017-12-21 09:39:22 -0800

    Redirected register to onboarding.

--- js/scenes/Login/Login.js
+++ js/scenes/Login/Login.js
@@ -1,5 +1,13 @@
 import React, { Component } from "react";
-import { KeyboardAvoidingView, Text, View, TextInput, TouchableOpacity, Image, ScrollView } from "react-native";
+import {
+  KeyboardAvoidingView,
+  Text,
+  View,
+  TextInput,
+  TouchableOpacity,
+  Image,
+  ScrollView
+} from "react-native";
 
 import styles from "./styles";
 import { goToRoute } from "../../lib/navigationHelpers";
@@ -10,29 +18,33 @@ const Login = ({ currentNavigatorUID, handleSubmit, email, password }) => {
   return (
     <KeyboardAvoidingView style={styles.container}>
       <View style={styles.loginHeaderWrapper}>
-        <Image source={require("../../assets/images/login/login-items.png")} style={styles.loginHeaderImage} resizeMode="contain"/>
-        <Image source={require("../../assets/images/echo-logo-white.png")} resizeMode="contain" />
+        <Image
+          source={require("../../assets/images/login/login-items.png")}
+          style={styles.loginHeaderImage}
+          resizeMode="contain"
+        />
+        <Image
+          source={require("../../assets/images/echo-logo-white.png")}
+          resizeMode="contain"
+        />
       </View>
 
       <View style={styles.loginContentWrapper}>
         <CustomSingleTextField placeholder="username" handler={email} />
         <CustomSingleTextField placeholder="password" handler={password} />
 
-        <TouchableOpacity
-          onPress={handleSubmit}
-          style={styles.logInButton}
-        >
+        <TouchableOpacity onPress={handleSubmit} style={styles.logInButton}>
           <CustomButton btnText="Log In" />
         </TouchableOpacity>
       </View>
-  
-        <View style={styles.bottomWrapper}>
-          <TouchableOpacity onPress={() => goToRoute("register")}>
-            <Text style={styles.textButton}>Register</Text>
-          </TouchableOpacity>
-        </View>
-      </KeyboardAvoidingView>
-    );
+
+      <View style={styles.bottomWrapper}>
+        <TouchableOpacity onPress={() => goToRoute("onboarding")}>
+          <Text style={styles.textButton}>Register</Text>
+        </TouchableOpacity>
+      </View>
+    </KeyboardAvoidingView>
+  );
 };
 
 export default Login;
